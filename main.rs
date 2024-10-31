aa33b40fa05f33c1b356d2259463bf15b5d12f3e
Danny <dannydotdev@gmail.com>
2024-10-31 10:28:49 +0100

    Key things to note as a beginner in rust

---
+++ main.rs
@@ -0,0 +1,21 @@
+/*
+START A RUST PROGRAM
+fn main ()
+this is the beginning of any rust program
+we declare the function using fn 
+
+PRINT
+println!("Hello, world!");
+that is how to print in rust
+
+VARAIBLES
+Assigned with the "let" keyword
+print! and println! are basically the same thing but the println! adds a new line
+
+SCOPE OF A VARIABLE
+is defined by the curly braces, ie the block of code in which it is declared
+
+FUNCTIONS are reuseable blocks of code
+
+SHADOWING allows a variabble to be redeclared with the same name in the same scope
+*/
